da13365ea72f9f62259aee4dbced3e630e81da8b
Vitaliy Yemets <vitali8.emez@gmail.com>
2019-11-19 23:39:18 +0200

    removed sample files

--- src/main/kotlin/com/mygroup/restApiTask/system/repositories.kt
+++
@@ -1,5 +0,0 @@
-package com.mygroup.restApiTask.system
-
-import org.springframework.data.repository.*
-
-interface ProductRepository : CrudRepository<Product, Long> // Дает нашему слою работы с данными весь набор CRUD операций
